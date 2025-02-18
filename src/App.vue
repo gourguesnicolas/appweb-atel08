608119315081a51e1067bdb94d6ee9293c0a17b0
gourguesnicolas <59837304+gourguesnicolas@users.noreply.github.com>
2025-02-18 14:52:15 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Timeline from "./components/Timeline.vue";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+</script>
+
+<template>
+  <div class="container">
+    <Timeline />
+  </div>
+</template>
+
+<style scoped>
+/* Style de l'application Vue */
+</style>
